338bf04e3d8ee16e15650ec9625835d202b4bc35
Charles Onunze <charlesonunze2@gmail.com>
2019-06-23 12:28:57 +0100

    rendered first component

---
+++ src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+	state = {};
+	render() {
+		return (
+			<div className='text-center'>
+				<span className='badge badge-info'>Counter component</span>
+			</div>
+		);
+	}
+}
+
+export default Counter;
